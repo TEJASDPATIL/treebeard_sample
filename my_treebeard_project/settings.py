f70318389613492eee2d2da21a23016bbdde4242
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-08-04 10:22:47 +0530

    treebeard project renamed to my_treebeard_project

--- my_treebeard_project/settings.py
+++ my_treebeard_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "treebeard",
+    "my_treebeard_project",
     "tree",
 ]
 
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'treebeard.urls'
+ROOT_URLCONF = 'my_treebeard_project.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'treebeard.wsgi.application'
+WSGI_APPLICATION = 'my_treebeard_project.wsgi.application'
 
 
 # Database
